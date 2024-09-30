74d5c2a99ad60483135e656198469ebd69f107e7
MartinVargas07 <martin.vargas@udla.edu.ec>
2024-09-30 17:02:08 -0500

    Añadir EF Core y configurar contexto y entidad Usuario
    Se han añadido referencias a los paquetes `Microsoft.EntityFrameworkCore.SqlServer` y `Microsoft.EntityFrameworkCore.Tools` en la versión 8.0.8 en el archivo `MVC_CRUD_VARGAS.csproj`.
    
    En el archivo `appsettings.json`, se ha añadido una nueva sección `ConnectionStrings` con una cadena de conexión vacía bajo el nombre `Conexion`.
    
    Se ha creado una nueva clase `MvcCrudContext` en el archivo `MvcCrudContext.cs` que hereda de `DbContext`. Esta clase configura el contexto de la base de datos para usar SQL Server y define un `DbSet` para la entidad `Usuario`.
    
    Se ha añadido una nueva clase `Usuario` en el archivo `Usuario.cs` que representa una entidad con propiedades `Id`, `Nombre`, `Fecha` y `Clave`.

---
+++ Models/MvcCrudContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_CRUD_VARGAS.Models;
+
+public partial class MvcCrudContext : DbContext
+{
+    public MvcCrudContext()
+    {
+    }
+
+    public MvcCrudContext(DbContextOptions<MvcCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=Martin-ROGSTRIX\\SQLEXPRESS; database=MVC_CRUD; integrated security=true; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC0707E36348");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
